29d065d574619a62c63771dd8529a0abcde26ae0
aymanhamoda <fullclinic22@gmail.com>
2021-12-13 18:27:22 +0200

    my changes

--- frontend/src/data/promotions.js
+++ frontend/src/data/promotions.js
@@ -2,18 +2,18 @@ const promotions = [
   {
     _id: '5fc2586c6fcf847fe32eb593',
     description: 'I hope that all my knowledge still in help',
-    image: '/uploads/promotion1.jpg',
+    image: '/uploads/images/promotion1.jpg',
   },
   {
     _id: '5fc258ce6fcf847fe32eb594',
     description:
       'I try to simplify and summarize ICU clinical pharmacy in intersting videos',
-    image: '/uploads/promotion2.jpg',
+    image: '/uploads/images/promotion2.jpg',
   },
   {
     _id: '5fc262216fcf847fe32eb595',
     description: 'ddddd',
-    image: '/uploads/promotion3.jpg',
+    image: '/uploads/images/promotion3.jpg',
   },
 ]
 export default promotions
